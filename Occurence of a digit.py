baf99aba665a91d5e742a30b86816511b7282207
naveenkumar30 <36320290+naveenkumar30@users.noreply.github.com>
2019-06-13 16:56:05 +0530

    Update Occurence of a digit.py

--- Occurence of a digit.py
+++ Occurence of a digit.py
@@ -1,3 +1,14 @@
-n=input()
-p=len(n)
-print(p)
+i=int(input())
+f=0
+c=0
+d=int(input())
+while (i>0):
+    r=i%10
+    if r==d:
+        c+=1
+        f=1
+    i=i//10
+if(f==0):
+    print("digit not found")
+else:
+    print("digit's count:",c)
