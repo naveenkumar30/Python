dc0c3ee82ff43ede364da6fa05fe45809d0cc454
naveenkumar30 <36320290+naveenkumar30@users.noreply.github.com>
2019-06-13 17:20:18 +0530

    Create array less than num.py

---
+++ array less than num.py
@@ -0,0 +1,12 @@
+arr=list(map(int,input()))
+n=int(input())
+p=0
+for i in range(0,len(arr)):
+    if(arr[i]<n):
+        print(arr[i],end=" ")
+arr=list(map(int,input()))
+n=int(input())
+p=0
+for i in range(0,len(arr)):
+    if(arr[i]<n):
+        print(arr[i],end=" ")
